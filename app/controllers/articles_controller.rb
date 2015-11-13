19bcf04be4320734c5f58fe9041d1d6544a83472
Esolam Kpemoua <esolamkpemoua@gmail.com>
2015-11-13 20:12:47 +0000

    Add association and debugger view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,15 +10,16 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    
+
   end
 
   def create
+   
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
     #@article.save
     #redirect_to article_path(@article)
-
+    @article.user = User.first
     if @article.save
       flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
@@ -28,7 +29,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    
+
     if @article.update(article_params)
       flash[:success] = "Article was successfully updated"
       redirect_to article_path(@article)
@@ -39,11 +40,11 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    
+
   end
 
   def destroy
-    
+
     @article.destroy
 
     flash[:danger] = "Article was successfully deleted"
